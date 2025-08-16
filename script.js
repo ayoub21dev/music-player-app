0ba7221873288a3bcdbd18dcc85e249633bc175b
Ayoub jalyta <ayoubcode941@gmail.com>
2025-08-16 12:17:28 +0100

    added playSong function

--- script.js
+++ script.js
@@ -124,3 +124,18 @@ const sortSongs = () => {
 
 
 renderSongs(sortSongs())
+
+const playSong = (id) => {
+  const song = userData?.songs.find((song) => song.id === id);
+  audio.src = song.src;
+  audio.title = song.title;
+
+  if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+    audio.currentTime = 0;
+  } else {
+    audio.currentTime = userData?.songCurrentTime;
+  }
+
+  userData.currentSong = song;
+
+};
